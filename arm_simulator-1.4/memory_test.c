dfcda752a1fbf0f0c95444d0d204c8ce0ea3d327
Frooztyes <yanis.miollany@gmail.com>
2021-01-05 14:29:13 +0100

    Start of reading memory's word

--- arm_simulator-1.4/memory_test.c
+++ arm_simulator-1.4/memory_test.c
@@ -94,6 +94,7 @@ int main() {
     printf("- word read with the same endianess as me, ");
     memory_read_word(m[is_big_endian()], 0, &word_read);
     print_test(compare(&word_value, &word_read, 4, 0));
+    printf("Word read : %d || Word value : %d\n", word_value, word_read);
     printf("- half read with the same endianess as me, ");
     memory_read_half(m[is_big_endian()], 0, &half_read);
     print_test(compare(&word_value, &half_read, 2, 0));

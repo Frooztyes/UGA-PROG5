a84b96e7fa00e56c093cf22ff720e96e70a1521f
Frooztyes <yanis.miollany@gmail.com>
2021-01-05 12:06:11 +0100

    Add start of memory read

--- arm_simulator-1.4/arm_core.c
+++ arm_simulator-1.4/arm_core.c
@@ -29,39 +29,46 @@ Contact: Guillaume.Huard@imag.fr
 #include "trace.h"
 #include <stdlib.h>
 
-struct arm_core_data {
+struct arm_core_data
+{
     uint32_t cycle_count;
     registers reg;
     memory mem;
 };
 
-arm_core arm_create(memory mem) {
+arm_core arm_create(memory mem)
+{
     arm_core p;
 
     p = malloc(sizeof(struct arm_core_data));
-    if (p) {
+    if (p)
+    {
         p->mem = mem;
-	p->reg = registers_create();
+        p->reg = registers_create();
         arm_exception(p, RESET);
         p->cycle_count = 0;
     }
     return p;
 }
 
-void arm_destroy(arm_core p) {
+void arm_destroy(arm_core p)
+{
     registers_destroy(p->reg);
     free(p);
 }
 
-int arm_current_mode_has_spsr(arm_core p) {
+int arm_current_mode_has_spsr(arm_core p)
+{
     return current_mode_has_spsr(p->reg);
 }
 
-int arm_in_a_privileged_mode(arm_core p) {
+int arm_in_a_privileged_mode(arm_core p)
+{
     return in_a_privileged_mode(p->reg);
 }
 
-uint32_t arm_get_cycle_count(arm_core p) {
+uint32_t arm_get_cycle_count(arm_core p)
+{
     return p->cycle_count;
 }
 
@@ -70,9 +77,11 @@ uint32_t arm_get_cycle_count(arm_core p) {
  * value of the pc is read, so that instructions read their own address + 8 when
  * reading the pc.
  */
-uint32_t arm_read_register(arm_core p, uint8_t reg) {
+uint32_t arm_read_register(arm_core p, uint8_t reg)
+{
     uint32_t value = read_register(p->reg, reg);
-    if (reg == 15) {
+    if (reg == 15)
+    {
         value += 4;
         value &= 0xFFFFFFFD;
     }
@@ -80,9 +89,11 @@ uint32_t arm_read_register(arm_core p, uint8_t reg) {
     return value;
 }
 
-uint32_t arm_read_usr_register(arm_core p, uint8_t reg) {
+uint32_t arm_read_usr_register(arm_core p, uint8_t reg)
+{
     uint32_t value = read_usr_register(p->reg, reg);
-    if (reg == 15) {
+    if (reg == 15)
+    {
         value += 4;
         value &= 0xFFFFFFFD;
     }
@@ -90,34 +101,40 @@ uint32_t arm_read_usr_register(arm_core p, uint8_t reg) {
     return value;
 }
 
-uint32_t arm_read_cpsr(arm_core p) {
+uint32_t arm_read_cpsr(arm_core p)
+{
     uint32_t value = read_cpsr(p->reg);
     trace_register(p->cycle_count, READ, CPSR, 0, value);
     return value;
 }
 
-uint32_t arm_read_spsr(arm_core p) {
+uint32_t arm_read_spsr(arm_core p)
+{
     uint32_t value = read_spsr(p->reg);
     trace_register(p->cycle_count, READ, SPSR, get_mode(p->reg), value);
     return value;
 }
 
-void arm_write_register(arm_core p, uint8_t reg, uint32_t value) {
+void arm_write_register(arm_core p, uint8_t reg, uint32_t value)
+{
     write_register(p->reg, reg, value);
     trace_register(p->cycle_count, WRITE, reg, get_mode(p->reg), value);
 }
 
-void arm_write_usr_register(arm_core p, uint8_t reg, uint32_t value) {
+void arm_write_usr_register(arm_core p, uint8_t reg, uint32_t value)
+{
     write_usr_register(p->reg, reg, value);
     trace_register(p->cycle_count, WRITE, reg, USR, value);
 }
 
-void arm_write_cpsr(arm_core p, uint32_t value) {
+void arm_write_cpsr(arm_core p, uint32_t value)
+{
     write_cpsr(p->reg, value);
     trace_register(p->cycle_count, WRITE, CPSR, 0, value);
 }
 
-void arm_write_spsr(arm_core p, uint32_t value) {
+void arm_write_spsr(arm_core p, uint32_t value)
+{
     write_spsr(p->reg, value);
     trace_register(p->cycle_count, WRITE, SPSR, get_mode(p->reg), value);
 }
@@ -126,7 +143,8 @@ void arm_write_spsr(arm_core p, uint32_t value) {
  * instruction being executed and the fetch increments the PC (this makes the
  * implementation of branches easier).
  */
-int arm_fetch(arm_core p, uint32_t *value) {
+int arm_fetch(arm_core p, uint32_t *value)
+{
     int result;
     uint32_t address;
 
@@ -138,7 +156,8 @@ int arm_fetch(arm_core p, uint32_t *value) {
     return result;
 }
 
-int arm_read_byte(arm_core p, uint32_t address, uint8_t *value) {
+int arm_read_byte(arm_core p, uint32_t address, uint8_t *value)
+{
     int result;
 
     result = memory_read_byte(p->mem, address, value);
@@ -149,7 +168,8 @@ int arm_read_byte(arm_core p, uint32_t address, uint8_t *value) {
 /* Data access endianess should comply with bit 9 of cpsr (E), see ARM
  * manual A4-129
  */
-int arm_read_half(arm_core p, uint32_t address, uint16_t *value) {
+int arm_read_half(arm_core p, uint32_t address, uint16_t *value)
+{
     int result;
 
     result = memory_read_half(p->mem, address, value);
@@ -157,7 +177,8 @@ int arm_read_half(arm_core p, uint32_t address, uint16_t *value) {
     return result;
 }
 
-int arm_read_word(arm_core p, uint32_t address, uint32_t *value) {
+int arm_read_word(arm_core p, uint32_t address, uint32_t *value)
+{
     int result;
 
     result = memory_read_word(p->mem, address, value);
@@ -165,7 +186,8 @@ int arm_read_word(arm_core p, uint32_t address, uint32_t *value) {
     return result;
 }
 
-int arm_write_byte(arm_core p, uint32_t address, uint8_t value) {
+int arm_write_byte(arm_core p, uint32_t address, uint8_t value)
+{
     int result;
 
     result = memory_write_byte(p->mem, address, value);
@@ -173,7 +195,8 @@ int arm_write_byte(arm_core p, uint32_t address, uint8_t value) {
     return result;
 }
 
-int arm_write_half(arm_core p, uint32_t address, uint16_t value) {
+int arm_write_half(arm_core p, uint32_t address, uint16_t value)
+{
     int result;
 
     result = memory_write_half(p->mem, address, value);
@@ -181,7 +204,8 @@ int arm_write_half(arm_core p, uint32_t address, uint16_t value) {
     return result;
 }
 
-int arm_write_word(arm_core p, uint32_t address, uint32_t value) {
+int arm_write_word(arm_core p, uint32_t address, uint32_t value)
+{
     int result;
 
     result = memory_write_word(p->mem, address, value);
@@ -189,40 +213,47 @@ int arm_write_word(arm_core p, uint32_t address, uint32_t value) {
     return result;
 }
 
-void arm_print_state(arm_core p, FILE *out) {
+void arm_print_state(arm_core p, FILE *out)
+{
     int mode, reg, count;
 
-    for (mode = 0; mode < 32; mode++) {
-        if (arm_get_mode_name(mode)) {
+    for (mode = 0; mode < 32; mode++)
+    {
+        if (arm_get_mode_name(mode))
+        {
             if (mode != SYS)
                 fprintf(out, "%s:", arm_get_mode_name(mode));
             count = 0;
-            for (reg=0; reg<16; reg++) {
-                if (mode == USR) {
-                        if ((reg > 0) && (reg%5 == 0))
-                            fprintf(out, "\n    ");
-                        fprintf(out, "   %3s=%08X", arm_get_register_name(reg),
-                                arm_read_usr_register(p, reg));
-                } else //if ((p->registers[mode][reg] - p->registers_storage) > 15) 
-                       {
-                        if ((count > 0) && (count%5 == 0))
-                            fprintf(out, "\n    ");
-                        count++;
-                        fprintf(out, "   %3s=%08X", arm_get_register_name(reg),
-                                arm_read_register(p, reg));
+            for (reg = 0; reg < 16; reg++)
+            {
+                if (mode == USR)
+                {
+                    if ((reg > 0) && (reg % 5 == 0))
+                        fprintf(out, "\n    ");
+                    fprintf(out, "   %3s=%08X", arm_get_register_name(reg),
+                            arm_read_usr_register(p, reg));
+                }
+                else //if ((p->registers[mode][reg] - p->registers_storage) > 15)
+                {
+                    if ((count > 0) && (count % 5 == 0))
+                        fprintf(out, "\n    ");
+                    count++;
+                    fprintf(out, "   %3s=%08X", arm_get_register_name(reg),
+                            arm_read_register(p, reg));
                 }
             }
             if (mode == USR)
                 fprintf(out, "  CPSR=%08X", arm_read_cpsr(p));
-            switch (mode) {
-              case USR:
-              case FIQ:
-              case SVC:
-              case UND:
+            switch (mode)
+            {
+            case USR:
+            case FIQ:
+            case SVC:
+            case UND:
                 fprintf(out, "\n");
                 break;
-              case IRQ:
-              case ABT:
+            case IRQ:
+            case ABT:
                 fprintf(out, "          ");
             }
         }
--- arm_simulator-1.4/memory.c
+++ arm_simulator-1.4/memory.c
@@ -10,6 +10,7 @@ GARANTIE, ni explicite ni implicite, y compris les garanties de
 commercialisation ou d'adaptation dans un but sp�cifique. Reportez-vous � la
 Licence Publique G�n�rale GNU pour plus de d�tails.
 
+<<<<<<< HEAD
 Vous devez avoir re�u une copie de la Licence Publique G�n�rale GNU en m�me
 temps que ce programme ; si ce n'est pas le cas, �crivez � la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
@@ -19,21 +20,43 @@ Contact: Guillaume.Huard@imag.fr
 	 B�timent IMAG
 	 700 avenue centrale, domaine universitaire
 	 38401 Saint Martin d'H�res
+=======
+Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
+temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
+États-Unis.
+
+Contact: Guillaume.Huard@imag.fr
+	 Bâtiment IMAG
+	 700 avenue centrale, domaine universitaire
+	 38401 Saint Martin d'Hères
+>>>>>>> Add start of memory read
 */
 #include <stdlib.h>
+#include <stdio.h>
 #include "memory.h"
 #include "util.h"
 #include <stdio.h>
 
 struct memory_data
 {
+<<<<<<< HEAD
     uint8_t *data;
     size_t taille;
     int be;
+=======
+    int be;
+    uint8_t *donnees;
+    size_t taille;
+>>>>>>> Add start of memory read
 };
 
 memory memory_create(size_t size, int is_big_endian)
 {
+<<<<<<< HEAD
+=======
+    fprintf(stderr, "La taille est de : %ld\n", size);
+>>>>>>> Add start of memory read
     memory mem = NULL;
     mem = malloc(sizeof(struct memory_data));
     if (mem == NULL)
@@ -42,8 +65,13 @@ memory memory_create(size_t size, int is_big_endian)
     }
     else
     {
+<<<<<<< HEAD
         mem->data = malloc(sizeof(uint8_t *) * size);
         if (mem->data == NULL)
+=======
+        mem->donnees = malloc(sizeof(uint8_t) * size);
+        if (mem->donnees == NULL)
+>>>>>>> Add start of memory read
         {
             free(mem);
             mem = NULL;
@@ -64,22 +92,46 @@ size_t memory_get_size(memory mem)
 
 void memory_destroy(memory mem)
 {
+<<<<<<< HEAD
     free(mem->data);
+=======
+    free(mem->donnees);
+>>>>>>> Add start of memory read
     free(mem);
 }
 
 int memory_read_byte(memory mem, uint32_t address, uint8_t *value)
 {
+<<<<<<< HEAD
     if(address < memory_get_size(mem)){
         *value = *(mem->data+address);
         return 0;
     } else {
         return -1;
+=======
+    if (mem->be == 0)
+    {
+        fprintf(stderr, "Value vaut : %d || ", *value);
+        *value = *(mem->donnees + address);        
+        fprintf(stderr, "L'adresse dans la mémoire vaut : %d || ", *(mem->donnees + address));
+        fprintf(stderr, "Value vaut : %d || ", *value);
+        return 0;
+    }
+    else if (mem->be == 1)
+    {
+        value = (mem->donnees + (mem->taille - address - 1));
+        return 0;
+    }
+    else
+    {
+        return 0;
+>>>>>>> Add start of memory read
     }
 }
 
 int memory_read_half(memory mem, uint32_t address, uint16_t *value)
 {
+<<<<<<< HEAD
     if(address < memory_get_size(mem)){
         *value = 0;
         uint8_t v = 0;
@@ -94,10 +146,14 @@ int memory_read_half(memory mem, uint32_t address, uint16_t *value)
     } else {
         return -1;
     }
+=======
+    return -1;
+>>>>>>> Add start of memory read
 }
 
 int memory_read_word(memory mem, uint32_t address, uint32_t *value)
 {
+<<<<<<< HEAD
     if(address < memory_get_size(mem)){
         *value = 0;
         uint8_t v = 0;
@@ -110,22 +166,85 @@ int memory_read_word(memory mem, uint32_t address, uint32_t *value)
         }
         return 0;
     } else {
+=======
+    if (mem->be == 0)
+    {
+        fprintf(stderr, "\n------------------------------------------\n\n");
+        fprintf(stderr, "Indien : %d || ", mem->be);
+        fprintf(stderr, "Byte : %i || ", address);
+        fprintf(stderr, "Taille : %ld || ", mem->taille);
+        fprintf(stderr, "L'adresse dans la mémoire avant est : %d\n\n", *(mem->donnees + (mem->taille - address - 1)));
+        
+        int i = 0;
+        uint8_t v = 0;
+        while(i < 4){
+            memory_read_byte(mem, i, &v);
+            fprintf(stderr, "Byte n°%i : %i\n", i, v);
+            i++;
+
+        }
+        fprintf(stderr, "\n\nL'adresse de base est : %ls", value);
+        fprintf(stderr, "L'adresse dans la mémoire est : %d\n", *(mem->donnees + address));
+        fprintf(stderr, "\n------------------------------------------\n\n");
+        return -1;
+    }
+    else if (mem->be == 1)
+    {
+        fprintf(stderr, "Indien : %d || ", mem->be);
+        fprintf(stderr, "Byte : %i || ", address);
+        fprintf(stderr, "Taille : %ld || ", mem->taille);
+        fprintf(stderr, "Byte BE : %ld || ", (mem->taille - address - 1));
+        fprintf(stderr, "L'adresse dans la mémoire avant est : %d || ", *(mem->donnees + (mem->taille - address - 1)));
+        fprintf(stderr, "L'adresse de base est : %ls || ", value);
+        // *(mem->donnees + address) = value;
+        fprintf(stderr, "L'adresse dans la mémoire est : %d\n", *(mem->donnees + address));
+        return -1;
+    }
+    else
+    {
+>>>>>>> Add start of memory read
         return -1;
     }
 }
 
 int memory_write_byte(memory mem, uint32_t address, uint8_t value)
 {
+<<<<<<< HEAD
     if(address < memory_get_size(mem)){
         *(mem->data + address) = value;
         return 0;
     } else {
+=======
+    if (mem->be == 0)
+    {
+        // fprintf(stderr, "L'adresse dans la mémoire avant est : %d || ", *(mem->donnees + address));
+        // fprintf(stderr, "L'adresse de base est : %d || ", value);
+        *(mem->donnees + address) = value;
+        // fprintf(stderr, "L'adresse dans la mémoire est : %d\n", *(mem->donnees + address));
+        return *(mem->donnees + address) == value ? 0 : -1;
+    }
+    else if (mem->be == 1)
+    {
+        // fprintf(stderr, "Indien : %d || ", mem->be);
+        // fprintf(stderr, "Byte : %d || ", address);
+        // fprintf(stderr, "Taille : %ld || ", mem->taille);
+        // fprintf(stderr, "Byte BE : %ld || ", (mem->taille - address - 1));
+        // fprintf(stderr, "L'adresse dans la mémoire avant est : %d || ", *(mem->donnees + (mem->taille - address - 1)));
+        // fprintf(stderr, "L'adresse de base est : %d || ", value);
+        *(mem->donnees + (mem->taille - address - 1)) = value;
+        // fprintf(stderr, "L'adresse dans la mémoire est : %d\n", *(mem->donnees + (mem->taille - address - 1)));
+        return *(mem->donnees + (mem->taille - address - 1)) == value ? 0 : -1;
+    }
+    else
+    {
+>>>>>>> Add start of memory read
         return -1;
     }
 }
 
 int memory_write_half(memory mem, uint32_t address, uint16_t value)
 {
+<<<<<<< HEAD
     if(address < memory_get_size(mem)){
         uint8_t value2 = 0;
         for (int i = address; i < sizeof(uint16_t); i++)
@@ -137,10 +256,14 @@ int memory_write_half(memory mem, uint32_t address, uint16_t value)
     } else {
         return -1;
     }
+=======
+    return -1;
+>>>>>>> Add start of memory read
 }
 
 int memory_write_word(memory mem, uint32_t address, uint32_t value)
 {
+<<<<<<< HEAD
     if(address < memory_get_size(mem)){
         uint8_t value2 = 0;
         for (int i = address; i < sizeof(uint32_t); i++)
@@ -150,6 +273,32 @@ int memory_write_word(memory mem, uint32_t address, uint32_t value)
         }
         return 0;
     } else {
+=======
+    if (mem->be == 0)
+    {
+        // fprintf(stderr, "L'adresse dans la mémoire avant est : %d || ", *(mem->donnees + address));
+        // fprintf(stderr, "L'adresse de base est : %d || ", value);
+        // *(mem->donnees + address) = value;
+        // fprintf(stderr, "L'adresse dans la mémoire est : %d\n", *(mem->donnees + address));
+        // return *(mem->donnees + address) == value ? 0 : -1;
+        return -1;
+    }
+    else if (mem->be == 1)
+    {
+        // fprintf(stderr, "Indien : %d || ", mem->be);
+        // fprintf(stderr, "Byte : %d || ", address);
+        // fprintf(stderr, "Taille : %ld || ", mem->taille);
+        // fprintf(stderr, "Byte BE : %ld || ", (mem->taille - address - 1));
+        // fprintf(stderr, "L'adresse dans la mémoire avant est : %d || ", *(mem->donnees + (mem->taille - address - 1)));
+        // fprintf(stderr, "L'adresse de base est : %d || ", value);
+        // *(mem->donnees + (mem->taille - address - 1)) = value;
+        // fprintf(stderr, "L'adresse dans la mémoire est : %d\n", *(mem->donnees + (mem->taille - address - 1)));
+        // return *(mem->donnees + (mem->taille - address - 1)) == value ? 0 : -1;
+        return -1;
+    }
+    else
+    {
+>>>>>>> Add start of memory read
         return -1;
     }
 }

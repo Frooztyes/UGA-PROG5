8accd78a98e6e7dbe17a3b358476f5a339c85e0c
Frooztyes <yanis.miollany@gmail.com>
2021-01-06 09:56:49 +0100

    add memory version

--- arm_simulator-1.4/memory.c
+++ arm_simulator-1.4/memory.c
@@ -21,7 +21,6 @@ Contact: Guillaume.Huard@imag.fr
 	 38401 Saint Martin d'H�res
 */
 #include <stdlib.h>
-#include <stdio.h>
 #include "memory.h"
 #include "util.h"
 #include <stdio.h>
@@ -33,6 +32,7 @@ struct memory_data
     int be;
 };
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 memory memory_create(size_t size, int is_big_endian)
 {
@@ -60,6 +60,11 @@ memory memory_create(size_t size, int is_big_endian)
     memory mem = NULL;
 >>>>>>> Start of reading memory's word
 >>>>>>> Start of reading memory's word
+=======
+memory memory_create(size_t size, int is_big_endian)
+{
+    memory mem = NULL;
+>>>>>>> add memory version
     mem = malloc(sizeof(struct memory_data));
     if (mem == NULL)
     {
@@ -69,7 +74,13 @@ memory memory_create(size_t size, int is_big_endian)
     {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> add memory version
         mem->data = malloc(sizeof(uint8_t *) * size);
+=======
+        mem->data = malloc(sizeof(uint8_t) * size);
+>>>>>>> add memory version
         if (mem->data == NULL)
 =======
         mem->donnees = malloc(sizeof(uint8_t) * size);
@@ -111,6 +122,7 @@ void memory_destroy(memory mem)
 }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 int memory_read_byte(memory mem, uint32_t address, uint8_t *value)
 {
 <<<<<<< HEAD
@@ -147,10 +159,13 @@ int memory_read_byte(memory mem, uint32_t address, uint8_t *value) {
     if(mem->be == 0){
         *value = *(mem->data + address);
 =======
+=======
+>>>>>>> add memory version
 int memory_read_byte(memory mem, uint32_t address, uint8_t *value)
 {
     if (mem->be == 0)
     {
+<<<<<<< HEAD
         // fprintf(stderr, "Value vaut : %d || ", *value);
         *value = *(mem->donnees + address);
         // fprintf(stderr, "L'adresse dans la mémoire vaut : %d || ", *(mem->donnees + address));
@@ -159,11 +174,24 @@ int memory_read_byte(memory mem, uint32_t address, uint8_t *value)
 >>>>>>> Start of reading memory's word
         return 0;
     } else {
+=======
+        *value = *(mem->data + address);
+        return 0;
+    }
+    else if (mem->be == 1)
+    {
+        *value = *(mem->data + (mem->taille - address - 1));
+        return 0;
+    }
+    else
+    {
+>>>>>>> add memory version
         return -1;
 >>>>>>> Début Arm Instruction
     }
 }
 
+<<<<<<< HEAD
 int memory_read_half(memory mem, uint32_t address, uint16_t *value)
 {
 <<<<<<< HEAD
@@ -235,12 +263,70 @@ int memory_read_half(memory mem, uint32_t address, uint16_t *value)
         }
         return 0;
     } else {
+<<<<<<< HEAD
 >>>>>>> Début Arm Instruction
+=======
+=======
+void mem_word_read_bytes(memory mem, uint32_t address, uint32_t *value)
+{
+    uint8_t v = 0;
+    uint32_t v2 = 0;
+    for (int i = 0; i < sizeof(uint32_t); i++)
+    {
+        memory_read_byte(mem, i, &v);
+        v2 = (uint32_t)v;
+        *value |= !mem->be ? (v2 << (8 * i)) : (v2 << (8 * (sizeof(uint32_t) - i - 1)));
+    }
+}
+
+void mem_half_read_bytes(memory mem, uint32_t address, uint16_t *value)
+{
+    uint8_t v = 0;
+    uint16_t v2 = 0;
+    for (int i = 0; i < sizeof(uint16_t); i++)
+    {
+        memory_read_byte(mem, i, &v);
+        v2 = (uint16_t)v;
+        *value |= !mem->be ? (v2 << (8 * i)) : (v2 << (8 * (sizeof(uint16_t) - i - 1)));
+    }
+}
+
+int memory_read_half(memory mem, uint32_t address, uint16_t *value)
+{
+    *value = 0;
+    mem_half_read_bytes(mem, address, &*value);
+    return 0;
+}
+
+int memory_read_word(memory mem, uint32_t address, uint32_t *value)
+{
+    *value = 0;
+    mem_word_read_bytes(mem, address, &*value);
+    return 0;
+}
+
+int memory_write_byte(memory mem, uint32_t address, uint8_t value)
+{
+    if (mem->be == 0)
+    {
+        *(mem->data + address) = value;
+        return 0;
+    }
+    else if (mem->be == 1)
+    {
+        *(mem->data + (mem->taille - address - 1)) = value;
+        return 0;
+    }
+    else
+    {
+>>>>>>> add memory version
+>>>>>>> add memory version
         return -1;
     }
 >>>>>>> add memory version
 }
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 int memory_read_word(memory mem, uint32_t address, uint32_t *value)
 {
@@ -328,26 +414,92 @@ int memory_read_word(memory mem, uint32_t address, uint32_t *value) {
 =======
 =======
 int memory_read_word(memory mem, uint32_t address, uint32_t *value)
+=======
+int memory_write_half(memory mem, uint32_t address, uint16_t value)
 {
-    if (mem->be == 0)
+    uint8_t value2 = 0;
+    for (int i = address; i < sizeof(uint16_t); i++)
     {
-        fprintf(stderr, "\n------------------------------------------\n\n");
-        fprintf(stderr, "Indien : %d || ", mem->be);
-        fprintf(stderr, "Byte : %i || ", address);
-        fprintf(stderr, "Taille : %ld\n", mem->taille);
+        value2 = !mem->be ? (value + i) >> 8 * i : (value + (sizeof(uint16_t) - i - 1)) >> 8 * (sizeof(uint16_t) - i - 1);
+        memory_write_byte(mem, i, value2);
+    }
+    return 0;
+}
 
-        int i = 0;
-        uint8_t v = 0;
-        uint32_t v2 = 0;
-        while (i < 4)
+//erreur value donne une valeur bizarre
+
+int memory_write_word(memory mem, uint32_t address, uint32_t value)
+>>>>>>> add memory version
+{
+    
+    uint8_t value2 = 0;
+    for (int i = address; i < sizeof(uint32_t); i++)
+    {
+        value2 = !mem->be ? (value + i) >> 8 * i : (value + (sizeof(uint32_t) - i - 1)) >> 8 * (sizeof(uint32_t) - i - 1);
+        memory_write_byte(mem, i, value2);
+    }
+    return 0;
+}
+/*
+Armator - simulateur de jeu d'instruction ARMv5T � but p�dagogique
+Copyright (C) 2011 Guillaume Huard
+Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
+termes de la Licence Publique G�n�rale GNU publi�e par la Free Software
+Foundation (version 2 ou bien toute autre version ult�rieure choisie par vous).
+
+Ce programme est distribu� car potentiellement utile, mais SANS AUCUNE
+GARANTIE, ni explicite ni implicite, y compris les garanties de
+commercialisation ou d'adaptation dans un but sp�cifique. Reportez-vous � la
+Licence Publique G�n�rale GNU pour plus de d�tails.
+
+Vous devez avoir re�u une copie de la Licence Publique G�n�rale GNU en m�me
+temps que ce programme ; si ce n'est pas le cas, �crivez � la Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
+�tats-Unis.
+
+Contact: Guillaume.Huard@imag.fr
+	 B�timent IMAG
+	 700 avenue centrale, domaine universitaire
+	 38401 Saint Martin d'H�res
+*/
+#include <stdlib.h>
+#include "memory.h"
+#include "util.h"
+#include <stdio.h>
+
+struct memory_data
+{
+    uint8_t *data;
+    size_t taille;
+    int be;
+};
+
+memory memory_create(size_t size, int is_big_endian)
+{
+    memory mem = NULL;
+    mem = malloc(sizeof(struct memory_data));
+    if (mem == NULL)
+    {
+        return NULL;
+    }
+    else
+    {
+        mem->data = malloc(sizeof(uint8_t) * size);
+        if (mem->data == NULL)
         {
-            memory_read_byte(mem, i, &v);
-            v2 = (uint32_t)v;
-            fprintf(stderr, "Bit: %d, Octet n°%i : %i\n", i * 8, i, v);
-            *value |= (v2 << (i * 8));
-            i++;
+            free(mem);
+            mem = NULL;
         }
+        else
+        {
+            mem->taille = size;
+            mem->be = is_big_endian;
+        }
+    }
+    return mem;
+}
 
+<<<<<<< HEAD
         fprintf(stderr, "\n\nLe mot est : %d", *value);
         fprintf(stderr, "\n------------------------------------------\n\n");
 >>>>>>> Start of reading memory's word
@@ -361,10 +513,42 @@ int memory_read_word(memory mem, uint32_t address, uint32_t *value)
 =======
     } else {
 >>>>>>> Début Arm Instruction
+=======
+size_t memory_get_size(memory mem)
+{
+    return mem->taille;
+}
+
+void memory_destroy(memory mem)
+{
+    free(mem->data);
+    free(mem);
+}
+
+int memory_read_byte(memory mem, uint32_t address, uint8_t *value)
+{
+    if (mem->be == 0)
+    {
+        *value = *(mem->data + address);
+        return 0;
+<<<<<<< HEAD
+    } else {
+=======
+    }
+    else if (mem->be == 1)
+    {
+        *value = *(mem->data + (mem->taille - address - 1));
+        return 0;
+    }
+    else
+    {
+>>>>>>> add memory version
+>>>>>>> add memory version
         return -1;
     }
 }
 
+<<<<<<< HEAD
 int memory_write_byte(memory mem, uint32_t address, uint8_t value)
 {
 <<<<<<< HEAD
@@ -413,6 +597,60 @@ int memory_write_half(memory mem, uint32_t address, uint16_t value)
         }
         return 0;
     } else {
+=======
+void mem_word_read_bytes(memory mem, uint32_t address, uint32_t *value)
+{
+    uint8_t v = 0;
+    uint32_t v2 = 0;
+    for (int i = 0; i < sizeof(uint32_t); i++)
+    {
+        memory_read_byte(mem, i, &v);
+        v2 = (uint32_t)v;
+        *value |= !mem->be ? (v2 << (8 * i)) : (v2 << (8 * (sizeof(uint32_t) - i - 1)));
+    }
+}
+
+void mem_half_read_bytes(memory mem, uint32_t address, uint16_t *value)
+{
+    uint8_t v = 0;
+    uint16_t v2 = 0;
+    for (int i = 0; i < sizeof(uint16_t); i++)
+    {
+        memory_read_byte(mem, i, &v);
+        v2 = (uint16_t)v;
+        *value |= !mem->be ? (v2 << (8 * i)) : (v2 << (8 * (sizeof(uint16_t) - i - 1)));
+    }
+}
+
+int memory_read_half(memory mem, uint32_t address, uint16_t *value)
+{
+    *value = 0;
+    mem_half_read_bytes(mem, address, &*value);
+    return 0;
+}
+
+int memory_read_word(memory mem, uint32_t address, uint32_t *value)
+{
+    *value = 0;
+    mem_word_read_bytes(mem, address, &*value);
+    return 0;
+}
+
+int memory_write_byte(memory mem, uint32_t address, uint8_t value)
+{
+    if (mem->be == 0)
+    {
+        *(mem->data + address) = value;
+        return 0;
+    }
+    else if (mem->be == 1)
+    {
+        *(mem->data + (mem->taille - address - 1)) = value;
+        return 0;
+    }
+    else
+    {
+>>>>>>> add memory version
         return -1;
     }
 =======
@@ -457,6 +695,7 @@ int memory_write_half(memory mem, uint32_t address, uint16_t value)
 >>>>>>> add memory version
 }
 
+<<<<<<< HEAD
 int memory_write_word(memory mem, uint32_t address, uint32_t value)
 {
 <<<<<<< HEAD
@@ -511,5 +750,29 @@ int memory_write_word(memory mem, uint32_t address, uint32_t value)
     } else {
 >>>>>>> Début Arm Instruction
         return -1;
+=======
+int memory_write_half(memory mem, uint32_t address, uint16_t value)
+{
+    uint8_t value2 = 0;
+    for (int i = address; i < sizeof(uint16_t); i++)
+    {
+        value2 = !mem->be ? (value + i) >> 8 * i : (value + (sizeof(uint16_t) - i - 1)) >> 8 * (sizeof(uint16_t) - i - 1);
+        memory_write_byte(mem, i, value2);
     }
+    return 0;
+}
+
+//erreur value donne une valeur bizarre
+
+int memory_write_word(memory mem, uint32_t address, uint32_t value)
+{
+    
+    uint8_t value2 = 0;
+    for (int i = address; i < sizeof(uint32_t); i++)
+    {
+        value2 = !mem->be ? (value + i) >> 8 * i : (value + (sizeof(uint32_t) - i - 1)) >> 8 * (sizeof(uint32_t) - i - 1);
+        memory_write_byte(mem, i, value2);
+>>>>>>> add memory version
+    }
+    return 0;
 }

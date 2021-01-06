33b76988acaaac148964e84fa8d96c16d5783585
Frooztyes <yanis.miollany@gmail.com>
2021-01-06 09:56:49 +0100

    add memory version

--- arm_simulator-1.4/memory.c
+++ arm_simulator-1.4/memory.c
@@ -10,7 +10,6 @@ GARANTIE, ni explicite ni implicite, y compris les garanties de
 commercialisation ou d'adaptation dans un but sp�cifique. Reportez-vous � la
 Licence Publique G�n�rale GNU pour plus de d�tails.
 
-<<<<<<< HEAD
 Vous devez avoir re�u une copie de la Licence Publique G�n�rale GNU en m�me
 temps que ce programme ; si ce n'est pas le cas, �crivez � la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
@@ -20,17 +19,6 @@ Contact: Guillaume.Huard@imag.fr
 	 B�timent IMAG
 	 700 avenue centrale, domaine universitaire
 	 38401 Saint Martin d'H�res
-=======
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
-
-Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
-	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
->>>>>>> Add start of memory read
 */
 #include <stdlib.h>
 #include <stdio.h>
@@ -40,15 +28,9 @@ Contact: Guillaume.Huard@imag.fr
 
 struct memory_data
 {
-<<<<<<< HEAD
     uint8_t *data;
     size_t taille;
     int be;
-=======
-    int be;
-    uint8_t *donnees;
-    size_t taille;
->>>>>>> Add start of memory read
 };
 
 memory memory_create(size_t size, int is_big_endian)
@@ -125,7 +107,7 @@ int memory_read_byte(memory mem, uint32_t address, uint8_t *value)
     }
     else if (mem->be == 1)
     {
-        value = (mem->donnees + (mem->taille - address - 1));
+        *value = *(mem->donnees + (mem->taille - address - 1));
         return 0;
     }
     else
@@ -137,6 +119,7 @@ int memory_read_byte(memory mem, uint32_t address, uint8_t *value)
 
 int memory_read_half(memory mem, uint32_t address, uint16_t *value)
 {
+<<<<<<< HEAD
 <<<<<<< HEAD
     if(address < memory_get_size(mem)){
         *value = 0;
@@ -155,10 +138,53 @@ int memory_read_half(memory mem, uint32_t address, uint16_t *value)
 =======
     return -1;
 >>>>>>> Add start of memory read
+=======
+    int i = 0;
+    *value = 0;
+    uint8_t v = 0;
+    uint16_t v2 = 0;
+    if (mem->be == 0)
+    {
+        // fprintf(stderr, "\n------------------------------------------\n\n");
+        // fprintf(stderr, "Indien : %d || ", mem->be);
+        // fprintf(stderr, "Byte : %i || ", address);
+        // fprintf(stderr, "Taille : %ld\n", mem->taille);
+
+        while (i < sizeof(uint16_t))
+        {
+            memory_read_byte(mem, i, &v);
+            v2 = (uint16_t)v;
+            // fprintf(stderr, "Bit: %d, Octet n°%i : %i\n", i * 8, i, v);
+            *value |= (v2 << (i * 8));
+            i++;
+        }
+
+        // fprintf(stderr, "\n\nLe mot est : %d", *value);
+        // fprintf(stderr, "\n------------------------------------------\n\n");
+        return 0;
+    }
+    else if (mem->be == 1)
+    {
+        while (i < sizeof(uint16_t))
+        {
+            memory_read_byte(mem, i, &v);
+            v2 = (uint16_t)v;
+            // fprintf(stderr, "Bit: %d, Octet n°%i : %i\n", i * 8, i, v);
+            *value |= (v2 << (8 * (sizeof(uint16_t) - i - 1)));
+            i++;
+        }
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+>>>>>>> add memory version
 }
 
 int memory_read_word(memory mem, uint32_t address, uint32_t *value)
 {
+<<<<<<< HEAD
 <<<<<<< HEAD
     if(address < memory_get_size(mem)){
         *value = 0;
@@ -173,40 +199,41 @@ int memory_read_word(memory mem, uint32_t address, uint32_t *value)
         return 0;
     } else {
 =======
+=======
+    int i = 0;
+    *value = 0;
+    uint8_t v = 0;
+    uint32_t v2 = 0;
+>>>>>>> add memory version
     if (mem->be == 0)
     {
-        fprintf(stderr, "\n------------------------------------------\n\n");
-        fprintf(stderr, "Indien : %d || ", mem->be);
-        fprintf(stderr, "Byte : %i || ", address);
-        fprintf(stderr, "Taille : %ld\n", mem->taille);
 
-        int i = 0;
-        uint8_t v = 0;
-        uint32_t v2 = 0;
-        while (i < 4)
+        while (i < sizeof(uint32_t))
         {
             memory_read_byte(mem, i, &v);
             v2 = (uint32_t)v;
-            fprintf(stderr, "Bit: %d, Octet n°%i : %i\n", i * 8, i, v);
+            // fprintf(stderr, "Bit: %d, Octet n°%i : %i\n", i * 8, i, v);
             *value |= (v2 << (i * 8));
             i++;
         }
 
-        fprintf(stderr, "\n\nLe mot est : %d", *value);
-        fprintf(stderr, "\n------------------------------------------\n\n");
+        // fprintf(stderr, "\n\nLe mot est : %d", *value);
+        // fprintf(stderr, "\n------------------------------------------\n\n");
         return 0;
     }
     else if (mem->be == 1)
     {
-        fprintf(stderr, "Indien : %d || ", mem->be);
-        fprintf(stderr, "Byte : %i || ", address);
-        fprintf(stderr, "Taille : %ld || ", mem->taille);
-        fprintf(stderr, "Byte BE : %ld || ", (mem->taille - address - 1));
-        fprintf(stderr, "L'adresse dans la mémoire avant est : %d || ", *(mem->donnees + (mem->taille - address - 1)));
-        fprintf(stderr, "L'adresse de base est : %ls || ", value);
-        // *(mem->donnees + address) = value;
-        fprintf(stderr, "L'adresse dans la mémoire est : %d\n", *(mem->donnees + address));
-        return -1;
+        while (i < sizeof(uint32_t))
+        {
+            memory_read_byte(mem, i, &v);
+            v2 = (uint32_t)v;
+            // fprintf(stderr, "Bit: %d, Octet n°%i : %i\n", i * 8, i, v);
+            *value |= (v2 << (8 * (sizeof(uint32_t) - i - 1)));
+            i++;
+        }
+
+        // fprintf(stderr, "\n\nLe mot est : %d", *value);
+        return 0;
     }
     else
     {
@@ -225,22 +252,12 @@ int memory_write_byte(memory mem, uint32_t address, uint8_t value)
 =======
     if (mem->be == 0)
     {
-        // fprintf(stderr, "L'adresse dans la mémoire avant est : %d || ", *(mem->donnees + address));
-        // fprintf(stderr, "L'adresse de base est : %d || ", value);
         *(mem->donnees + address) = value;
-        // fprintf(stderr, "L'adresse dans la mémoire est : %d\n", *(mem->donnees + address));
         return *(mem->donnees + address) == value ? 0 : -1;
     }
     else if (mem->be == 1)
     {
-        // fprintf(stderr, "Indien : %d || ", mem->be);
-        // fprintf(stderr, "Byte : %d || ", address);
-        // fprintf(stderr, "Taille : %ld || ", mem->taille);
-        // fprintf(stderr, "Byte BE : %ld || ", (mem->taille - address - 1));
-        // fprintf(stderr, "L'adresse dans la mémoire avant est : %d || ", *(mem->donnees + (mem->taille - address - 1)));
-        // fprintf(stderr, "L'adresse de base est : %d || ", value);
         *(mem->donnees + (mem->taille - address - 1)) = value;
-        // fprintf(stderr, "L'adresse dans la mémoire est : %d\n", *(mem->donnees + (mem->taille - address - 1)));
         return *(mem->donnees + (mem->taille - address - 1)) == value ? 0 : -1;
     }
     else
@@ -252,6 +269,7 @@ int memory_write_byte(memory mem, uint32_t address, uint8_t value)
 
 int memory_write_half(memory mem, uint32_t address, uint16_t value)
 {
+<<<<<<< HEAD
 <<<<<<< HEAD
     if(address < memory_get_size(mem)){
         uint8_t value2 = 0;
@@ -267,10 +285,42 @@ int memory_write_half(memory mem, uint32_t address, uint16_t value)
 =======
     return -1;
 >>>>>>> Add start of memory read
+=======
+    int i = 0;
+    uint16_t value2 = 0;
+    if (mem->be == 0)
+    {
+        while (i < sizeof(uint16_t))
+        {
+            value2 = (value + i) >> 8 * i;
+            // fprintf(stderr, "Nb F : %d, Bit: %d, Octet n°%i : %d\n", (i+1), i * 8, i, value2);
+            memory_write_byte(mem, i, value2);
+            i++;
+        }
+        return 0;
+    }
+    else if (mem->be == 1)
+    {
+        while (i < sizeof(uint16_t))
+        {
+            int decal = (sizeof(uint16_t) - i - 1);
+            value2 = (value + decal) >> 8 * decal;
+            // fprintf(stderr, "Nb F : %d, Bit: %d, Octet n°%i : %d\n", (i+1), i * 8, i, value2);
+            memory_write_byte(mem, i, value2);
+            i++;
+        }
+        return -1;
+    }
+    else
+    {
+        return -1;
+    }
+>>>>>>> add memory version
 }
 
 int memory_write_word(memory mem, uint32_t address, uint32_t value)
 {
+<<<<<<< HEAD
 <<<<<<< HEAD
     if(address < memory_get_size(mem)){
         uint8_t value2 = 0;
@@ -282,26 +332,29 @@ int memory_write_word(memory mem, uint32_t address, uint32_t value)
         return 0;
     } else {
 =======
+=======
+    int i = 0;
+    uint32_t value2 = 0;
+>>>>>>> add memory version
     if (mem->be == 0)
     {
-        // fprintf(stderr, "L'adresse dans la mémoire avant est : %d || ", *(mem->donnees + address));
-        // fprintf(stderr, "L'adresse de base est : %d || ", value);
-        // *(mem->donnees + address) = value;
-        // fprintf(stderr, "L'adresse dans la mémoire est : %d\n", *(mem->donnees + address));
-        // return *(mem->donnees + address) == value ? 0 : -1;
+        while (i < sizeof(uint32_t))
+        {
+            value2 = (value + i) >> 8 * i;
+            memory_write_byte(mem, i, value2);
+            i++;
+        }
         return -1;
     }
     else if (mem->be == 1)
     {
-        // fprintf(stderr, "Indien : %d || ", mem->be);
-        // fprintf(stderr, "Byte : %d || ", address);
-        // fprintf(stderr, "Taille : %ld || ", mem->taille);
-        // fprintf(stderr, "Byte BE : %ld || ", (mem->taille - address - 1));
-        // fprintf(stderr, "L'adresse dans la mémoire avant est : %d || ", *(mem->donnees + (mem->taille - address - 1)));
-        // fprintf(stderr, "L'adresse de base est : %d || ", value);
-        // *(mem->donnees + (mem->taille - address - 1)) = value;
-        // fprintf(stderr, "L'adresse dans la mémoire est : %d\n", *(mem->donnees + (mem->taille - address - 1)));
-        // return *(mem->donnees + (mem->taille - address - 1)) == value ? 0 : -1;
+        while (i < sizeof(uint32_t))
+        {
+            int decal = (sizeof(uint32_t) - i - 1);
+            value2 = (value + decal) >> 8 * decal;
+            memory_write_byte(mem, i, value2);
+            i++;
+        }
         return -1;
     }
     else

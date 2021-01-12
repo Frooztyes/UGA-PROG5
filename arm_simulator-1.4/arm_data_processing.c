000516144c83de1e6536db2d0d015ed63f604fe8
Frooztyes <yanis.miollany@gmail.com>
2021-01-12 10:38:27 +0100

    data processing code v2

--- arm_simulator-1.4/arm_data_processing.c
+++ arm_simulator-1.4/arm_data_processing.c
@@ -27,6 +27,8 @@ Contact: Guillaume.Huard@imag.fr
 #include "util.h"
 #include "debug.h"
 
+#define ADD = 1;
+#define SUB = 0;
 
 uint32_t get_rs_bits(arm_core p, uint32_t rs, uint8_t max, uint8_t min) {
 	return get_bits(arm_read_register(p, rs), max, min);
@@ -36,34 +38,65 @@ uint32_t get_rm_bit(arm_core p, uint32_t rm, uint8_t ind) {
 	return get_bit(arm_read_register(p, rm), ind);
 }
 
-uint32_t get_C_flag(arm_core p) {
+int get_N_flag(arm_core p) {
+	return get_bit(arm_read_cpsr(p), 31);
+}
+
+int get_Z_flag(arm_core p) {
+	return get_bit(arm_read_cpsr(p), 30);
+}
+
+int get_C_flag(arm_core p) {
 	return get_bit(arm_read_cpsr(p), 29);
 }
 
+int get_V_flag(arm_core p) {
+	return get_bit(arm_read_cpsr(p), 28);
+}
 
-void shifter_value_calculator(uint32_t *shift_value, arm_core p, uint8_t I, uint8_t shift_op, uint8_t Rs, uint8_t Rm, uint32_t shift_imm){
+int carryFrom(uint64_t res){
+	return res >> 32;
+}
+
+int overflowFrom(uint32_t operande1, uint32_t operande2, uint64_t res, int op){
+	if(op == ADD){
+		if(get_bit(operande1, 31) == get_bit(operande2,31) && get_bit(operande1, 31) != get_bit(res)){
+			return 1;
+		}else if(get_bit(operande1,31) != get_bit(operande) && get_bit(operande1,31) != get_bit(res)){
+			return 1;
+		}
+		return 0;
+	}
+}
+
+
+void shifter_value_calculator(uint32_t *shift_value, uint32_t *shifter_carry_out, arm_core p, uint8_t I, uint8_t shift_op, uint8_t Rs, uint8_t Rm, uint32_t shift_imm){
 	uint32_t Rs_reg, Rm_reg = arm_read_register(p, Rm);
+	uint32_t rm_31 = get_rm_bit(p, Rm, 31);
 
+	
 	if(I){
 		Rs_reg = arm_read_register(p, Rs);
 		uint32_t rs_7_0 = get_rs_bits(p, Rs, 7, 0);
-		uint32_t rm_31 = get_rm_bit(p, Rm, 31);
 		uint32_t rs_4_0 = get_rs_bits(p, Rs, 4, 0);
 		switch (shift_op)
 		{
 			case LSL: // Logical shift left
 				if(rs_7_0 == 0) {
 					*shift_value = Rm_reg;
-					// shifter carry out = C Flag
+					*shifter_carry_out = get_C_flag(p);
 				} else if( rs_7_0 < 32) {
 					*shift_value = Rm_reg << rs_7_0;
 					// shifter carry out = Rm_reg[Rs[7:0] - 1]
+					*shifter_carry_out = get_rm_bit(p, Rm_reg, rs_7_0 - 1);
 				}else if(rs_7_0 == 32){
 					*shift_value = 0;
 					// shifter carry out = Rm_reg[31]
+					*shifter_carry_out = rm_31;
 				}else{
 					*shift_value = 0;
 					// shifter carry out = 0
+					*shifter_carry_out = 0;
 				}
 				break;
 
@@ -71,15 +104,19 @@ void shifter_value_calculator(uint32_t *shift_value, arm_core p, uint8_t I, uint
 				if(rs_7_0 == 0){
 					*shift_value = Rm_reg;
 					// shifter_carry_out = C Flags
+					*shifter_carry_out = get_C_flag(p);
 				}else if(rs_7_0 < 32){
 					*shift_value = Rm_reg >> rs_7_0;
 					// shifter_carry_out = Rm[Rs[7:0] - 1]
+					*shifter_carry_out = get_rm_bit(p, Rm_reg, rs_7_0 - 1);
 				}else if(rs_7_0 == 32){
 					*shift_value = 0;
 					// shifter carry out = Rm_reg[31]
+					*shifter_carry_out = rm_31;
 				}else{
 					*shift_value = 0;
 					//shifter carry out = 0
+					*shifter_carry_out = 0;
 				}
 				break;
 
@@ -88,16 +125,20 @@ void shifter_value_calculator(uint32_t *shift_value, arm_core p, uint8_t I, uint
 				if(Rs_reg == 0) {
 					*shift_value = Rm_reg;
 					// shifter_carry_out = C Flags
+					*shifter_carry_out = get_C_flag(p);
 				} else if(Rs_reg < 32) {
 					*shift_value = asr(Rm_reg, Rs_reg);
 					// shifter_carry_out = Rm[Rs[7:0] - 1]
+					*shifter_carry_out = get_rm_bit(p, Rm_reg, rs_7_0 - 1);
 				} else if(Rs_reg >= 32) {
 					if(rm_31 == 1) {
 						*shift_value = 0;
 						// shifter carry out = Rm_reg[31]
+						*shifter_carry_out = rm_31;
 					}  else {
 						*shift_value = 0xFFFFFFFF;
 						//shifter carry out = 0
+						*shifter_carry_out = 0;
 					}
 				}
 				break;
@@ -106,12 +147,15 @@ void shifter_value_calculator(uint32_t *shift_value, arm_core p, uint8_t I, uint
 				if(rs_7_0 == 0) {
 					*shift_value = Rm_reg;
 					// shifter_carry_out = C Flags
+					*shifter_carry_out = get_C_flag(p);
 				}else if(rs_4_0 == 0){
 					*shift_value = Rm_reg;
 					// shifter_carry_out = Rm[31]
+					*shifter_carry_out = rm_31;
 				} else if(rs_4_0 > 0) {
 					*shift_value = ror(Rm_reg, rs_4_0);
 					// shifter_carry_out = Rm[Rs[4:0] - 1]
+					*shifter_carry_out = get_rm_bit(p, Rm_reg, rs_4_0 - 1);
 				}
 				break;
 			
@@ -127,9 +171,11 @@ void shifter_value_calculator(uint32_t *shift_value, arm_core p, uint8_t I, uint
 				if(shift_imm == 0) {
 					*shift_value = Rm;
 					// shifter carry out = Rm_reg[31];
+					*shifter_carry_out = rm_31;
 				} else {
 					*shift_value = Rm_reg << shift_imm;
 					// shifter carry out = Rm_reg[31 - shift_imm]
+					*shifter_carry_out = get_rm_bit(p, Rm_reg, 31 - shift_imm);
 				}
 				break;
 
@@ -137,33 +183,41 @@ void shifter_value_calculator(uint32_t *shift_value, arm_core p, uint8_t I, uint
 				if(shift_imm == 0){
 					*shift_value = 0;
 					// shifter_carry_out = Rm[31]
+					*shifter_carry_out = rm_31;
 				}else {
 					*shift_value = Rm >> shift_imm;
 					// shifter_carry_out = Rm[shift_imm - 1];
+					*shifter_carry_out = get_rm_bit(p, Rm_reg, shift_imm - 1);
 				}
 				break;
 
 			case ASR: // Logical shift right
-				if (shift_imm == 0)
-					if (get_rm_bit (p, Rm, 31) == 0)
+				if (shift_imm == 0) {
+					if (get_rm_bit (p, Rm, 31) == 0) {
 						*shift_value = 0;
 						//shifter_carry_out = Rm[31]
-					else /* Rm[31] == 1 */
+						*shifter_carry_out = rm_31;
+					} else { /* Rm[31] == 1 */
 						*shift_value = 0xFFFFFFFF;
 						//shifter_carry_out = Rm[31]
-				else /* shift_imm > 0 */
+						*shifter_carry_out = rm_31;
+					}
+				} else {/* shift_imm > 0 */
 					*shift_value = asr(Rm, shift_imm);
 					//shifter_carry_out = Rm[shift_imm - 1]
-
+					*shifter_carry_out = get_rm_bit(p, Rm_reg, shift_imm - 1);
+				}
 				break;
 
 			case ROR: // Logical shift right
 				if(shift_imm == 0){
-					shift_op = (get_C_flag(p) << 31) || (Rm >> 1);
+					shift_op = ( ((uint32_t) get_C_flag(p)) << 31) || (Rm >> 1);
 					//shifter_carry_out = Rm[0]
+					*shifter_carry_out = get_rm_bit(p, Rm_reg, 0);
 				}else{
 					*shift_value = ror(Rm_reg, shift_imm);
 					//shifter_carry_out = Rm[shift_imm - 1];
+					*shifter_carry_out = get_rm_bit(p, Rm_reg, shift_imm - 1);
 				}
 				break;
 			
@@ -171,11 +225,32 @@ void shifter_value_calculator(uint32_t *shift_value, arm_core p, uint8_t I, uint
 				*shift_value = Rm_reg;
 				break;
 		}
-		
 		// data pro immediate shift  
 	}
 }
 
+void teq_process(arm_core p,uint8_t rn,uint32_t shift_value){
+	
+	// Update flags after Rn EOR shifter_operand
+
+	uint32_t reg_new, rn;
+	int N, Z, V, C;
+
+	rn = arm_read_register(p,rn);
+
+	reg_new = rn ^ shift_value;
+	
+	// Get new flags value
+	N = get_bit(reg_new, 31);
+	Z = if reg_new == 0 then 1 else 0;
+	V = get_V_flag();
+	C = shifter_carry_out;
+	
+	// Update flags
+	update_flags(p, Z, N, C, V);
+	
+	
+}
 
 void and_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value) {
 	// Rd := Rn AND shifter_operand
@@ -186,13 +261,24 @@ void and_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value) {
 	arm_write_register(p, rd, reg_new);
 }
 
-void eor_processing(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value,  int s)
+void eor_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value,  int s)
 {
 	//Rd := Rn EOR shifter_operand
 	uint32_t reg_new;
 	
 	reg_new = arm_read_register(p,rn) ^ shift_value;
     arm_write_register(p,rd, reg_new);
+
+	if(s == 1 && rd == 15){
+		if(arm_current_mode_has_spsr(p)){
+			arm_write_cpsr(p, arm_read_spsr(p));
+		}else{
+			//return UNDEFINED_INSTRUCTION;
+		}
+	} 
+	else if(s == 1){
+		teq_process(p,rn,shift_value);
+	}
 }
 
 void substract_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value, int s) {
@@ -204,7 +290,6 @@ void substract_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value,
 	
 }
 
-
 void reverse_sub_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value, int s){
 	// Rd := shifter_operand - Rn
 	uint32_t reg_new;
@@ -215,18 +300,60 @@ void reverse_sub_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_valu
 
 void add_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value, int s){
 	// Rd = Rn + Shift_value
+	uint8_t z,n,c,v;
 	uint32_t reg_new;
-	
 	reg_new = arm_read_register(p,rn) + shift_value;
-    arm_write_register(p,rd, reg_new);
+	arm_write_register(p,rd, reg_new);
+	// pour le S je sais pas trop ce que c'est pas contre, S = ?? (a quoi il correspond)
+	//moi non plus
+	if(s == 1 && rd == 15){
+		if(arm_current_mode_has_spsr(p)){
+			arm_write_cpsr(p, arm_read_spsr(p));
+		}else{
+			//return UNDEFINED_INSTRUCTION;
+		}
+	} else if(s == 1){
+		if(rd == 0){
+			z = 1;
+		}else{
+			z = 0;
+		}
+		n = get_bit(reg_new, 31);
+		c = carryFrom(reg_new);
+		v = overflowFrom(arm_read_register(p,rn),shift_value,reg_new,ADD);
+	}
+		
 }
+
+/*
+A revoir surtout pour OverflowFrom c'est un peu bizarre -> Vincent
+*/
 
 void adc_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value, int s){
 	// Rd = Rn + Shift_value + Carry Flag
+	uint8_t z,n,c,v;
 	uint32_t reg_new;
+	reg_new = arm_read_register(p,rn) + shift_value + get_C_flag(p);
+	arm_write_register(p,rd, reg_new);
+	if(s == 1 && rd == 15){
+		if(arm_current_mode_has_spsr(p)){
+			arm_write_cpsr(p, arm_write_spsr(p));
+		}else{
+			//UNPREDICTABLE
+		}
+	}else if(s == 1){
+		if(rd == 0){
+			z = 1;
+		}else{
+			z = 0;
+		}
+		n = get_bit(reg_new, 31);
+		c = carryFrom(reg_new);
+		v = overflowFrom(arm_read_register(p,rn), shift_value + get_C_flag(p), reg_new, ADD);
+	}
 	
 	reg_new = arm_read_register(p,rn) + shift_value + get_C_flag(p);
-    arm_write_register(p,rd, reg_new);
+    
 }
 
 void sbc_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value, int s){
@@ -277,14 +404,212 @@ void mvn_process(arm_core p, uint8_t rd, uint32_t shift_value, int s){
     arm_write_register(p,rd, reg_new);
 }
 
+void comp_process(arm_core p, uint8_t rn, uint32_t shift_value){
+	
+	/*
+	alu_out = Rn - shifter_operand
+	N Flag = alu_out[31]
+	Z Flag = if alu_out == 0 then 1 else 0
+	C Flag = NOT BorrowFrom(Rn - shifter_operand)
+	V Flag = OverflowFrom(Rn - shifter_operand)
+	*/
 
-/* Decoding functions for different classes of instructions */
+	uint32_t reg_new, op1;
+	uint8_t z = 0, n = 0, c = 0, v = 0;
+	
+	op1 = arm_read_register(p, rn);
+	reg_new = op1 - shift_value;
+
+	if (get_bit(reg_new,31) == 1)
+		n = 1;
+	if (reg_new == 0)
+		z = 1;
+	if (get_bit(op1, 31) == get_bit(shift_value,31) && get_bit(op1,31) != get_bit(reg_new,31)))
+		v = 1
+	if (OverflowFrom(op1, reg_new, SUB))
+		c = 1
+
+	//on update les flags celon les erreurs
+	update_flags(p, z, n, c, v);
+
+	arm_write_register(p, reg_new);
+	
+	
+	
+}
+
+void update_flags(arm_core p, uint8_t z, uint8_t n, uint8_t c, uint8_t v){
+
+	//étape 1 : récup le registre cpsr
+	uint32_t new_cpsr, Old_cpsr = arm_read_cpsr(p);
+
+    //étape 2 : effacer les anciennes valeurs des flags dans le registre
+    Old_cpsr = Old_cpsr & ~(0xf << 28);
+
+	//étape 3 : modifier les valeurs du ou des bits n correspondant au flag
+    if (z) 
+		new_cpsr = set_bit(Old_cpsr, Z); 
+    if (n) 
+		new_cpsr = set_bit(Old_cpsr, N);
+    if (c) 
+		new_cpsr = set_bit(Old_cpsr, C);
+    if (v) 
+		new_cpsr = set_bit(Old_cpsr, V);
+    
+    //étape 4 : écrire dans le registre cpsr
+    arm_write_cpsr(p, new_cpsr);
+
+	
+	/*
+	uint32_t decalN = (1<<31);
+	uint32_t decalZ = (1<<30);
+	uint32_t decalC = (1<<29);
+	uint32_t decalV = (1<<28);
+	uint32_t OldCpsr = arm_read_cpsr(p);
+
+	//flag Z
+	if(reg == 0)
+		arm_write_cpsr(p, (OldCpsr | decalZ));
+	else
+		arm_write_cpsr(p, (OldCpsr & (~decalZ)));
+	
+
+	//flag N
+	if (get_bit(reg,31) == 1)
+		arm_write_cpsr(p, (OldCpsr | decalN));
+	else
+		arm_write_cpsr(p, (OldCpsr & (~decalN)));
+
+
+	//flag C
+	arm_write_cpsr(p, (OldCpsr & (~decalC)));
+
+
+	//flag V
+	if(get_bit(operande_1,31) == get_bit(val_sh,31) && get_bit(operande_1,31) != get_bit(reg,31))
+		arm_write_cpsr(p, (OldCpsr | decalV));
+	else
+		arm_write_cpsr(p, (OldCpsr & (~decalV)));
+*/
+}
+
+void compN_process(arm_core p, uint8_t rn, uint32_t shift_value){
+	
+}
+
+int cond_verifier(arm_core p, uint32_t cond) {
+
+	switch (cond)
+	{
+		case 0:
+			// EQ
+			// Z set
+			return get_Z_flag(p);
+			break;
+
+		case 1:
+			// NE
+			// Z clear
+			return !get_Z_flag(p);
+			break;
+
+		case 2:
+			// CS/HS
+			// C set
+			return get_C_flag(p);
+			break;
+			
+		case 3:
+			// CC/LO
+			// C clear
+			return !get_C_flag(p);
+			break;
+
+		case 4:
+			// MI
+			// N set
+			return get_N_flag(p);
+			break;
+
+		case 5:
+			// PL
+			// N clear
+			return !get_N_flag(p);
+			break;
+
+		case 6:
+			// VS
+			// V set
+			return get_C_flag(p);
+			break;
+
+		case 7:
+			// VC
+			// V clear
+			return !get_C_flag(p);
+			break;
+
+		case 8:
+			// HI Unsigned higher 
+			// C set and Z clear
+			return get_C_flag(p) && get_Z_flag(p);
+			break;
+		
+		case 9:
+			// LS Unsigned lower or equal
+			// C clear or Z set
+			return !get_C_flag(p) && get_Z_flag(p);
+			break;
+
+		case 10:
+			// GE signed greater than or equal 
+			// (N set and V set) or (N clear and V clear)
+			return (get_N_flag(p) && get_V_flag(p)) || (!get_N_flag(p) && !get_V_flag(p));
+			break;
+
+		case 11:
+			// LT 
+			// (N set and V clear) or (N clear and V set)
+			return (get_N_flag(p) && !get_V_flag(p)) || (!get_N_flag(p) && get_V_flag(p));
+			break;
+
+		case 12:
+			// GT signed greater than
+			// Z clear and either N set and V set or (N clear and V clear)
+			return !get_Z_flag(p) && ((get_N_flag(p) && get_V_flag(p)) || (!get_N_flag(p) && !get_Z_flag(p)));
+			break;
+
+		case 13:
+			// LE signed less than or equal
+			// (N set and V clear) or (N clear and V set)
+			return get_Z_flag(p) ||  ((get_N_flag(p) && !get_V_flag(p)) || (!get_N_flag(p) && !get_V_flag(p)));
+			break;
+
+		case 14:
+			// AL Always (unconditionnal)
+			// true
+			return 1;
+			break;
+
+		case 15:
+			// -
+			// UNPREDICTABLE
+			return -1;
+			break;
+
+		default:
+			break;
+	}
+	return 0;
+}
+
 int arm_data_processing_shift(arm_core p, uint32_t ins) {
 	fprintf(stderr, "In arm_data_processing_shift\n");
-	int /*cond,*/ rn, rd, opcode, s;
+	int cond, rn, rd, opcode, s;
 	uint8_t shift_amnt, rm, rs, I, shift_op;
-	uint32_t shift_value;
+	uint32_t shift_value, shifter_carry_out;
 
+	// parlez sur discord c'est plus simple jav
 	opcode = get_bits(ins, 24, 21);
 
 	// destination register
@@ -295,100 +620,103 @@ int arm_data_processing_shift(arm_core p, uint32_t ins) {
 	shift_amnt = get_bits(ins, 11, 7);
 	rs = get_bits(ins, 11, 6);
 	rm = get_bits(ins, 3, 0);
-	shifter_value_calculator(&shift_value, p, I, shift_op, rs, rm, shift_amnt);
+	shifter_value_calculator(&shift_value, &shifter_carry_out, p, I, shift_op, rs, rm, shift_amnt);
 
 	
 	rn = get_bits(ins, 19, 16);
 	s = get_bit(ins, 20);
-	// cond = get_bits(ins, 31, 28);
+	cond = get_bits(ins, 31, 28);
+	
+	if(cond_verifier(p, cond)) {
+		switch (opcode)
+		{
+			case 0:
+				// Logical AND
+				// Yanis
+				and_process(p, rn, rd, shift_value);
+				break;
 
+			case 1:
+				// Logical exclusive OR
+				// NiCONla
+				eor_process(p, rn, rd, shift_value, s);
+				break;
 
-	switch (opcode)
-	{
-		case 0:
-			// Logical AND
-			// Yanis
-			and_process(p, rn, rd, shift_value);
-			break;
+			case 2:
+				// Substract
+				// Mathis
+				substract_process(p, rn, rd, shift_value, s);
+				break;
 
-		case 1:
-			// Logical exclusive OR
-			// NiCON
-			eor_processing(p, rn, rd, shift_value, s);
-			break;
+			case 3: // rémi le bg
+				rsc_process(p, rn, rd, shift_value, s);
+				// Reverse substract
+				break;
 
-		case 2:
-			// Substract
-			// Mathis
-			substract_process(p, rn, rd, shift_value, s);
-			break;
+			case 4:
+				// Add
+				add_process(p, rn, rd, shift_value, s);
+				break;
 
-		case 3: // rémi le bg
-			rsc_process(p, rn, rd, shift_value, s);
-			// Reverse substract
-			break;
+			case 5:
+				// Add with carry
+				adc_process(p, rn, rd, shift_value, s);
+				break;
 
-		case 4:
-			// Add
-			add_process(p, rn, rd, shift_value, s);
-			break;
+			case 6:
+				// Substract with carry
+				sbc_process(p, rn, rd, shift_value, s);
+				break;
 
-		case 5:
-			// Add with carry
-			adc_process(p, rn, rd, shift_value, s);
-			break;
+			case 7:
+				// Reverse substract with carry
+				rsc_process(p, rn, rd, shift_value, s);
+				break;
 
-		case 6:
-			// Substract with carry
-			sbc_process(p, rn, rd, shift_value, s);
-			break;
+			case 8:
+				// Test
+				break;
 
-		case 7:
-			// Reverse substract with carry
-			rsc_process(p, rn, rd, shift_value, s);
-			break;
+			case 9:
+				// Test equivalence
+				break;
 
-		case 8:
-			// Test
-			break;
+			case 10:
+				//A faire	
+				// Compare 
+				comp_process(p, rn, shift_value);
+				break;
+				
+			case 11:
+				//A faire
+				// Compare negated
+				compN_process(p, rn, shift_value);
+				break;
 
-		case 9:
-			// Test equivalence
-			break;
+			case 12:
+				// Logical (inclusive) OR
+				orr_process(p, rn, rd, shift_value, s);
+				break;
 
-		case 10:
-			// Compare
-			break;
-			
-		case 11:
-			// Compare negated
-			break;
+			case 13:
+				// Move
+				mov_process(p, rd, shift_value, s);
+				break;
 
-		case 12:
-			// Logical (inclusive) OR
-			orr_process(p, rn, rd, shift_value, s);
-			break;
+			case 14:
+				// Bit clear
+				bic_process(p, rn, rd, shift_value, s);
+				break;
 
-		case 13:
-			// Move
-			mov_process(p, rd, shift_value, s);
-			break;
-
-		case 14:
-			// Bit clear
-			bic_process(p, rn, rd, shift_value, s);
-			break;
-
-		case 15:
-			// Move not
-			mvn_process(p, rd, shift_value, s);
-			break;
+			case 15:
+				// Move not
+				mvn_process(p, rd, shift_value, s);
+				break;
+		}
 	}
-
     return UNDEFINED_INSTRUCTION;
 }
 
 int arm_data_processing_immediate_msr(arm_core p, uint32_t ins) {
     return UNDEFINED_INSTRUCTION;
 }
-
--- arm_simulator-1.4/arm_data_processing.h
+++ arm_simulator-1.4/arm_data_processing.h
@@ -1,31 +1,113 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #ifndef __ARM_DATA_PROCESSING_H__
 #define __ARM_DATA_PROCESSING_H__
 #include <stdint.h>
 #include "arm_core.h"
 
+
+/* Decoding functions for different classes of instructions 
+*/
 int arm_data_processing_shift(arm_core p, uint32_t ins);
 int arm_data_processing_immediate_msr(arm_core p, uint32_t ins);
 
+/* RÃ©cupÃ¨re la valeur du registre Rs entre les bornes max et min 
+*/
+uint32_t get_rs_bits(arm_core p, uint32_t rs, uint8_t max, uint8_t min);
+
+/* RÃ©cupÃ¨re la valeur du registre Rs au bit ind
+*/
+uint32_t get_rm_bit(arm_core p, uint32_t rm, uint8_t ind);
+
+/* RÃ©cupÃ¨re le flag N dans le registre CPSR au bit 31
+*/
+int get_N_flag(arm_core p);
+
+/* RÃ©cupÃ¨re le flag Z dans le registre CPSR au bit 30
+*/
+int get_Z_flag(arm_core p);
+
+/* RÃ©cupÃ¨re le flag C dans le registre CPSR au bit 29
+*/
+int get_C_flag(arm_core p);
+
+/* RÃ©cupÃ¨re le flag V dans le registre CPSR au bit 28
+*/
+int get_V_flag(arm_core p);
+
+/* Modifie la valeur de shift_value et de shifter_carry_out grÃ¢ce aux diffÃ©rentes valeurs de l'instruction
+*/
+void shifter_value_calculator(uint32_t *shift_value, uint32_t *shifter_carry_out, arm_core p, uint8_t I, uint8_t shift_op, uint8_t Rs, uint8_t Rm, uint32_t shift_imm);
+
+/* TODO : DESCRIPTION */
+void teq_process(arm_core p,uint8_t rn,uint32_t val_sh);
+
+/* TODO : DESCRIPTION */
+void and_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value);
+
+/* TODO : DESCRIPTION */
+void eor_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value,  int s);
+
+/* TODO : DESCRIPTION */
+void substract_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value, int s);
+
+/* TODO : DESCRIPTION */
+void reverse_sub_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value, int s);
+
+/* TODO : DESCRIPTION */
+void add_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value, int s);
+
+/* TODO : DESCRIPTION */
+void adc_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value, int s);
+
+/* TODO : DESCRIPTION */
+void sbc_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value, int s);
+
+/* TODO : DESCRIPTION */
+void rsc_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value, int s);
+
+/* TODO : DESCRIPTION */
+void orr_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value, int s);
+
+/* TODO : DESCRIPTION */
+void mov_process(arm_core p, uint8_t rd, uint32_t shift_value, int s);
+
+/* TODO : DESCRIPTION */
+void bic_process(arm_core p, uint8_t rn, uint8_t rd, uint32_t shift_value, int s);
+
+/* TODO : DESCRIPTION */
+void mvn_process(arm_core p, uint8_t rd, uint32_t shift_value, int s);
+
+/* TODO : DESCRIPTION */
+void comp_processing(arm_core p, uint8_t rn, uint32_t shift_value);
+
+/* TODO : DESCRIPTION */
+void compN_processing(arm_core p, uint8_t rn, uint32_t shift_value);
+
+/* VÃ©rifie les valeurs des diffÃ©rents flags Z, N, V et C
+en fonction des bits cond (31 Ã  28) dans l'instruction.
+*/
+int cond_verifier(arm_core p, uint32_t cond);
+
 #endif
+ 
